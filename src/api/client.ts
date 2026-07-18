78a62480e0c40c784930acd97fdb1d972b0156f3
Sezar204 <Supersezar20@gmail.com>
2026-07-18 19:45:53 +0300

    Update client.ts

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,3 +1,5 @@
+
+Input
 import axios, { AxiosInstance } from "axios"
 
 const client: AxiosInstance = axios.create({
@@ -18,15 +20,15 @@ client.interceptors.response.use(
 
 export const api = {
   get:    async <T>(url: string, params?: object) =>
-    (await client.get<T>(url, { params })).data,
+            (await client.get<T>(url, { params })).data,
   post:   async <T>(url: string, data?: object) =>
-    (await client.post<T>(url, data)).data,
+            (await client.post<T>(url, data)).data,
   put:    async <T>(url: string, data?: object) =>
-    (await client.put<T>(url, data)).data,
+            (await client.put<T>(url, data)).data,
   patch:  async <T>(url: string, data?: object) =>
-    (await client.patch<T>(url, data)).data,
+            (await client.patch<T>(url, data)).data,
   delete: async <T>(url: string) =>
-    (await client.delete<T>(url)).data,
+            (await client.delete<T>(url)).data,
   upload: async <T>(url: string, file: File, extra?: object) => {
     const fd = new FormData()
     fd.append("file", file)
